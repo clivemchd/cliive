0f0b9dc1706d91541a8e519e3749ad725845d701
clivemchd <clivemchd@gmail.com>
2025-07-25 10:59:19 +0200

    feat: deployment for pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/', // Use '/' for custom domain, change to '/repository-name/' if using username.github.io/repository-name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
   server: {
     host: true,
     port: 3000
